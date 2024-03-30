37ca69fe2d8b8b807d50ea918a1fcd58a849bf61
vadim <neobut93@gmail.com>
2024-03-29 20:07:39 -0700

    added filled start animation

--- app/src/main/java/com/kodeco/android/countryinfo/ui/components/CountryInfoRow.kt
+++ app/src/main/java/com/kodeco/android/countryinfo/ui/components/CountryInfoRow.kt
@@ -1,18 +1,31 @@
 package com.kodeco.android.countryinfo.ui.components
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.kodeco.android.countryinfo.R
 import com.kodeco.android.countryinfo.models.Country
 import com.kodeco.android.countryinfo.sample.sampleCountry
 
@@ -22,6 +35,9 @@ fun CountryInfoRow(
     country: Country,
     onTap: () -> Unit,
 ) {
+    var iconState by remember {
+        mutableStateOf(false)
+    }
     Card(
         onClick = onTap,
         modifier = Modifier
@@ -37,9 +53,21 @@ fun CountryInfoRow(
                 Text(text = "Name: ${country.commonName}")
                 Text(text = "Capital: ${country.capital?.firstOrNull() ?: "N/A"}")
             }
-            // TODO: Add favorite icon/button or custom composable
+            //todo add animation for spinning + cache
+            val color by animateColorAsState(targetValue = if (iconState) Color.Yellow else Color.Black)
+            val icon by animateIntAsState(targetValue = if (iconState) R.drawable.star_filled else R.drawable.star_outline)
+            Image(
+                painter = painterResource(icon),
+                contentDescription = "starIcon",
+                colorFilter = ColorFilter.tint(color),
+                modifier = Modifier
+                    .size(30.dp)
+                    .padding(end = 5.dp)
+                    .clickable {
+                        iconState = !iconState
+                    }
+            )
         }
-
     }
 }
 
