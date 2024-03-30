85dc37564b208f0c9c97059397e70687df7970be
vadim <neobut93@gmail.com>
2024-03-30 11:07:44 -0700

    added animations for area and population rows

--- app/src/main/java/com/kodeco/android/countryinfo/ui/components/CountryDetails.kt
+++ app/src/main/java/com/kodeco/android/countryinfo/ui/components/CountryDetails.kt
@@ -2,10 +2,10 @@ package com.kodeco.android.countryinfo.ui.components
 
 import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.Transition
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.animateInt
-import androidx.compose.animation.core.animateValue
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.border
@@ -41,7 +41,13 @@ fun CountryDetails(
     var isExpandedFlag by remember {
         mutableStateOf(false)
     }
-    var isTextChanged by remember {
+    var isCapitalChanged by remember {
+        mutableStateOf(false)
+    }
+    var isPopulationChanged by remember {
+        mutableStateOf(false)
+    }
+    var isAreaChanged by remember {
         mutableStateOf(false)
     }
     val flagSize by animateDpAsState(
@@ -51,45 +57,53 @@ fun CountryDetails(
             easing = LinearEasing
         ), label = "flag size"
     )
-    val transition = updateTransition(targetState = isTextChanged)
-    val textColor by transition.animateColor(
-        transitionSpec = { tween(500)},
-        label = "textColor",
-        targetValueByState = { isTextChanged ->
-            if(isTextChanged) Color.Red else Color.Black
-        }
-    )
-    val textFont by transition.animateInt(
-        transitionSpec = { tween(500)},
-        label = "textColor",
-        targetValueByState = { isTextChanged ->
-            if(isTextChanged) 700 else 400
-        }
-    )
-    val textRotation by transition.animateFloat(
-        transitionSpec = { tween(500)},
-        label = "rotation",
-        targetValueByState = {isTextChanged ->
-            if(isTextChanged) 360f else 0f }
-    )
-
-
+    val capitalTransition = updateTransition(targetState = isCapitalChanged)
+    val populationTransition = updateTransition(targetState = isPopulationChanged)
+    val areaTransition = updateTransition(targetState = isAreaChanged)
 
     LazyColumn(modifier = modifier) {
-        item { Text(
-            text = "Capital: ${country.mainCapital}",
-            color = textColor,
-            modifier = Modifier
-                .graphicsLayer(
-                    rotationY = textRotation
-                )
-                .clickable {
-                isTextChanged = !isTextChanged
-            },
-            fontWeight = FontWeight(textFont)
-        ) }
-        item { Text(text = "Population: ${country.population}") }
-        item { Text(text = "Area: ${country.area}") }
+        item {
+            Text(
+                text = "Capital: ${country.mainCapital}",
+                color = textColor(capitalTransition),
+                modifier = Modifier
+                    .graphicsLayer(
+                        rotationY = textRotation(transition = capitalTransition)
+                    )
+                    .clickable {
+                        isCapitalChanged = !isCapitalChanged
+                    },
+                fontWeight = FontWeight(textWeight(transition = capitalTransition))
+            )
+        }
+        item {
+            Text(
+                text = "Population: ${country.population}",
+                color = textColor(populationTransition),
+                modifier = Modifier
+                    .graphicsLayer(
+                        rotationY = textRotation(transition = populationTransition)
+                    )
+                    .clickable {
+                        isPopulationChanged = !isPopulationChanged
+                    },
+                fontWeight = FontWeight(textWeight(transition = populationTransition))
+            )
+        }
+        item {
+            Text(
+                text = "Area: ${country.area}",
+                color = textColor(areaTransition),
+                modifier = Modifier
+                    .graphicsLayer(
+                        rotationY = textRotation(transition = areaTransition)
+                    )
+                    .clickable {
+                        isAreaChanged = !isAreaChanged
+                    },
+                fontWeight = FontWeight(textWeight(transition = areaTransition))
+            )
+        }
         item {
             AsyncImage(
                 model = ImageRequest.Builder(LocalContext.current)
@@ -105,6 +119,42 @@ fun CountryDetails(
             )
         }
     }
+}
+
+@Composable
+private fun textColor(transition: Transition<Boolean>): Color {
+    val textColor by transition.animateColor(
+        transitionSpec = { tween(500) },
+        label = "textColor",
+        targetValueByState = { isTextChanged ->
+            if (isTextChanged) Color.Red else Color.Black
+        }
+    )
+    return textColor
+}
+
+@Composable
+private fun textWeight(transition: Transition<Boolean>): Int {
+    val textFont by transition.animateInt(
+        transitionSpec = { tween(500) },
+        label = "textColor",
+        targetValueByState = { isTextChanged ->
+            if (isTextChanged) 700 else 400
+        }
+    )
+    return textFont
+}
+
+@Composable
+private fun textRotation(transition: Transition<Boolean>): Float {
+    val textRotation by transition.animateFloat(
+        transitionSpec = { tween(500) },
+        label = "rotation",
+        targetValueByState = { isTextChanged ->
+            if (isTextChanged) 360f else 0f
+        }
+    )
+    return textRotation
 }
 
 @Preview
